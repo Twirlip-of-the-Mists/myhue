79f66ab08a0a4c7340425e0762f748db46932b4e
Twirlip-of-the-Mists <Sandor.at.the.Zoo@gmail.com>
2021-02-16 21:09:45 +0100

    Fixed some indentation

--- src/myhue/formats.py
+++ src/myhue/formats.py
@@ -8,13 +8,13 @@ import dicttoxml
 INDENT = 2 # FIXME This should be an option
 
 def python_print(name, data, indent=INDENT):
-  pprint.pprint(data, indent=indent)
+    pprint.pprint(data, indent=indent)
 
 def json_print(name, data, indent=INDENT):
-  print(json.dumps(data, indent=indent))
+    print(json.dumps(data, indent=indent))
   
 def yaml_print(name, data, indent=INDENT):
-  print(yaml.safe_dump(data, indent=indent))
+    print(yaml.safe_dump(data, indent=indent))
 
 def xml_list_item(list_name):
     if list_name == 'colorgamut':
@@ -27,10 +27,10 @@ def xml_list_item(list_name):
         return 'item'
     
 def xml_print(name, data, indent=INDENT):
-  xml = dicttoxml.dicttoxml(data, custom_root=name,
-                            attr_type=False, item_func=xml_list_item)
-  dom = parseString(xml)
-  print(dom.toprettyxml(indent=' '*indent))
+    xml = dicttoxml.dicttoxml(data, custom_root=name,
+                              attr_type=False, item_func=xml_list_item)
+    dom = parseString(xml)
+    print(dom.toprettyxml(indent=' '*indent))
 
 formats = {
     'XML':xml_print,
